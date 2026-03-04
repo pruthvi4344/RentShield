94e5531295c4e45408ca3f54cceceb72f85fc8aa
Your Name <ramamsaini1297@gmail.com>
2026-03-04 16:56:17 -0500

    upadted

--- app/dashboard/owner/page.tsx
+++ app/dashboard/owner/page.tsx
@@ -3,8 +3,6 @@ import OwnerHeader from "@/components/OwnerHeader";
 export default function OwnerDashboard() {
   return (
     <>
-      <OwnerHeader />
-
       <div style={{ padding: 40 }}>
         <h1>Owner Dashboard</h1>
       </div>
